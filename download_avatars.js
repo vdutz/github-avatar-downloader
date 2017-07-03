6aff58557c7871f53d701b0cdea859e108928987
Vlad Duta <vlad.du09@gmail.com>
2017-07-03 18:02:12 +0000

    Added code to formulate the URL being requested based on my github username and token

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,15 @@ var request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
+var GITHUB_USER = 'vdutz';
+var GITHUB_TOKEN = '5ff676298d5b86d9aa9cd5fea7d66079a41f9195'
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL)
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
