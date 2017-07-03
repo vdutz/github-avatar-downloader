ea7a47b16557a3ac21e50877b09e9f7255919821
Vlad Duta <vlad.du09@gmail.com>
2017-07-03 21:37:18 +0000

    Cleaned up comments

--- download_avatars.js
+++ download_avatars.js
@@ -16,7 +16,6 @@ var GITHUB_USER = 'vdutz';
 var GITHUB_TOKEN = '5ff676298d5b86d9aa9cd5fea7d66079a41f9195'
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
 
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
